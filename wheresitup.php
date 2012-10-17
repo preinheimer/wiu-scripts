86bb779a509149954729d558c01f791334f1304d
Paul Reinheimer <preinheimer@debian.localdomain>
2012-10-16 22:18:26 -0400

    - Adding some documentation

--- wheresitup.php
+++ wheresitup.php
@@ -3,6 +3,24 @@
 define('clientID', 'YOUR CLIENT ID HERE, alternately: sadness');
 define('token', 'Auth tokens are tasty, like cookies, but without the calories');
 
+/*
+ *  You're clearly welcome to edit what comes later, but you shouldn't need to.
+ *
+ *  A few notes:
+ *      - The script uses a sleep(5) before submitting the job and checking the first
+ *      time, and then before every subsequent check. This is a long time in computing
+ *      worlds, but remember that the server has to do a lot of work. Dropping this to
+ *      zero is a great way to get throttled.
+ *      - http_build_query() wories about escaping for us
+ *      - Designed to integrate with Nagios it uses return codes 0, 2, and 3. Which
+ *      represent Success, Failure, and Unknown respectively. This simple implementaiton
+ *      doesn't present warn.
+ *      - When the check fails it immediately requests a second check against that URL,
+ *      including Traceroute, Ping, and DNS lookups. While the results of these lookups
+ *      aren't available in the echo'd result, the link to them is. This should help
+ *      diagnose problems.
+ */
+
 if(!isset($argv[1]) OR !isset($argv[2]))
 {
     echo "Usage: {$argv[0]} <server name> <check source> [accept redirects]\n";
@@ -71,6 +89,7 @@ do {
     if($attemptCount++ == 10)
     {
         //Something may be wrong with the API
+        echo "Where's it Up not responding!";
         exit(3);
         $done = true;
     }
